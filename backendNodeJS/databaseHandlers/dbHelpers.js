50a3707563d6e0e261cac76c1ad1b59cd5a80aef
Salman <rahman_5@live.ca>
2019-01-28 01:44:18 -0500

    Staged the dbHelper file; it didn't get uploaded before

---
+++ backendNodeJS/databaseHandlers/dbHelpers.js
@@ -0,0 +1,23 @@
+// importing mongoose
+const mongoose = require('mongoose');
+
+// the exports object can be set to functions that needs to be exposed to other files
+var exports = module.exports = {};
+
+
+exports.connectToDB = function (databaseName, mongoDbHost,mongoDbHostParam) {
+
+  // the following parameters creates the mongoDb url to connect to
+  // when the dataBaseName is changed a new database will be created and connected to from the code!
+  const mongoDbHostURL = mongoDbHost + databaseName + mongoDbHostParam + mongoDbHost;
+
+  // connect to database using the url from the website
+  mongoose.connect(mongoDbHostURL, {useNewUrlParser: true})
+    .then(() => {
+      console.log('connected to the db');
+    })
+    .catch(() => {
+      console.log('connection failed!');
+    });
+
+}
