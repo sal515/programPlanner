59b788516011763ee242af3b17522bb18d27267f
leiandros <leiandros@hotmail.com>
2019-03-12 12:47:48 -0400

    Added basic user schema to DbSchemas directory

---
+++ backendNodeJS/models/DbSchemas/userSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+  // userID should be 8 characters
+  userID: {type: Number, required: true},
+  // userPass is case sensitive
+  userPass: {type: Number, required: true},
+  // sequence should tell user when they started and if coop. Example: Fall start year 2 SOEN
+  sequence: {type: String,required: true},
+  // course should include catalog number and subject for now: ELEC 311
+  courseHistory: {type: [String], required: true},
+  // courseCredits example: 33.5   --> Note: In JS float are also referred as of type "Number"
+  completedCredits: {type: Number, required: true}
+});
+
+// The following takes the definition and models for data storage etc
+// mongoose.model("dbModelName", mongooseSchema);
+
+// The following is a model created from the layout created above
+// The model has to be exported, so that it can be used outside of the file
+// module.exports = mongoose.model("dbModelName", mongooseSchema);
+module.exports = userSchema;
