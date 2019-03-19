89c9f6f86436f3331aba058ad520cc9b3609246a
Unknown <claudia.lapalme@gmail.com>
2019-03-19 09:16:27 -0400

    forgot a file

--- backendNodeJS/models/loginSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const loginSchema = mongoose.Schema({
-
-    loginUsername: {type: String, required: true},
-    loginPassword: {type: String, required: true}
-
-});
-
-module.exports = mongoose.model("loginInfo", loginSchema);
