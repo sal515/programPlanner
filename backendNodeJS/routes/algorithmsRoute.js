6acdbd1f2f6e87691c578b197108445d3295a832
Salman <rahman_5@live.ca>
2019-04-04 14:11:27 -0400

    PopulateSidebar with data from Backend

--- backendNodeJS/routes/algorithmsRoute.js
+++ backendNodeJS/routes/algorithmsRoute.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const algorithm = require('../controllers/algorithmsController');
+// const algorithm = require('../controllers/addCourseController');
+// router.post("/addCourseToSequence", algorithm.addCourseToSequence);
 
-router.post("/addCourseToSequence", algorithm.addCourseToSequence);
+const addCourseController = require('../controllers/addCourseController');
+const populateSidebarController = require('../controllers/populateSidebarController');
+
+
+router.post("/addCourseToSequence", addCourseController.addCourseToSequence);
+router.get("/getCourses", populateSidebarController.populateSidebar);
+
+
 
 module.exports = router;
