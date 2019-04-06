09aff50328e32bfc782586e276b38c7899a1b6ad
Colin Barber <colinbmusic@gmail.com>
2019-04-06 14:27:02 -0400

    code cleanup

--- src/app/authentication-service-guards/authenticationGuard/authentication.guard.ts
+++ src/app/authentication-service-guards/authenticationGuard/authentication.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import {AuthenticationService} from "../authentication.service";
+import {AuthenticationService} from '../authentication.service';
 
 @Injectable({
   providedIn: 'root'
@@ -18,15 +18,11 @@ export class AuthenticationGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
 
-    if(this.authService.isLoggedIn())
-    {
-      return true
-    }
-    else
-    {
-      this.router.navigate(["/login"]);
-      return false
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
     }
   }
-
 }
