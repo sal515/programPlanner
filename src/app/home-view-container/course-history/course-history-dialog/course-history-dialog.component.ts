a00daf608929c2fbaa092bfdcbfecde9fd62751b
Alessandro <aleoriskiraly@gmail.com>
2019-04-11 12:54:58 -0400

    Course History
    Course history done.

---
+++ src/app/home-view-container/course-history/course-history-dialog/course-history-dialog.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-history-dialog',
+  templateUrl: './course-history-dialog.component.html',
+  styleUrls: ['./course-history-dialog.component.css']
+})
+export class CourseHistoryDialogComponent{
+
+  courseHistoryData = CourseHistoryDialogComponent.getCourseHistory();
+  semesters = CourseHistoryDialogComponent.getSemestersCourseHistory();
+
+  static getCourseHistory()
+  {
+    let studentProfile = JSON.parse(localStorage.getItem('studentProfile'));
+    let courseHistory = studentProfile.courseHistory;
+    let semestersArray : string[] = [];
+    for (let semester in courseHistory){
+        semestersArray.push(semester);
+    }
+    return studentProfile.courseHistory;
+  }
+
+  static getSemestersCourseHistory()
+  {
+    let studentProfile = JSON.parse(localStorage.getItem('studentProfile'));
+    let courseHistory = studentProfile.courseHistory;
+    let semestersArray : string[] = [];
+    for (let semester in courseHistory){
+      semestersArray.push(semester);
+    }
+    return semestersArray;
+  }
+
+
+}
